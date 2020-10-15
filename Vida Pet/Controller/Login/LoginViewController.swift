2d98df02431646bc02125fa6a3894e2d979fa26c
João Pedro Giarrante <joaopedrogiarrante15@gmail.com>
2020-10-15 02:35:01 -0300

    add: adicionando controllers, no padrão MVC

---
+++ Vida Pet/Controller/Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Vida Pet
+//
+//  Created by João Pedro Giarrante on 15/10/20.
+//  Copyright © 2020 João Pedro Giarrante. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: VidaPetMainViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Vida Pet/Controller/Recem Chegados/RecemChegadosListaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecemChegadosListaViewController.swift
+//  Vida Pet
+//
+//  Created by João Pedro Giarrante on 15/10/20.
+//  Copyright © 2020 João Pedro Giarrante. All rights reserved.
+//
+
+import UIKit
+
+class RecemChegadosListaViewController: VidaPetMainViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

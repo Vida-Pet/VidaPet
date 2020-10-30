67cf19bb893e93a5e760d80946cdee3f5d7b7110
Bruna Ebina <bruebina@gmail.com>
2020-10-30 18:37:18 -0300

    Adicionei layout no storyboard

--- Vida Pet/Controller/Perfil/PerfilViewController.swift
+++ Vida Pet/Controller/Perfil/PerfilViewController.swift
@@ -10,20 +10,30 @@ import UIKit
 
 class PerfilViewController: VidaPetMainViewController {
 
+    
+    @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var petsLabel: UILabel!
+    @IBOutlet weak var regiaoLabel: UILabel!
+    @IBOutlet weak var inicioLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageButton.setImage(UIImage(named: "userImage.png"), for: .normal)
+        imageButton.imageView?.contentMode = .scaleAspectFit
+        imageButton.imageView?.layer.cornerRadius = (imageButton.imageView?.frame.width)! / 2
+        imageButton.imageView?.layer.borderColor = #colorLiteral(red: 0.1842333972, green: 0.7304695249, blue: 0.7287064195, alpha: 1)
+        imageButton.imageView?.layer.borderWidth = 2.0
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func imagePressed(_ sender: UIButton) {
+        print("image pressed")
     }
-    */
-
 }

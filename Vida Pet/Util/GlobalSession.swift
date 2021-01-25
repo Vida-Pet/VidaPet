d5e3b26a8571d310027bb85c0ca163ea50df6f3e
brunofreire239 <freire_ageu@hotmail.com>
2021-01-25 19:39:31 -0300

    Adicionado metodos para retornar email e senha.

--- Vida Pet/Util/GlobalSession.swift
+++ Vida Pet/Util/GlobalSession.swift
@@ -13,6 +13,8 @@ class GlobalSession{
     
     static let userKey = "userId"
     static let userUid = "userUid"
+    static let emailKey = "email"
+    static let pwdKey = "pwd"
     
     public static func getUserId() -> Int? {
         return UserDefaults.standard.integer(forKey: userKey)
@@ -26,4 +28,21 @@ class GlobalSession{
         UserDefaults.standard.set(userId, forKey: userKey)
         UserDefaults.standard.set(uid, forKey: userUid)
     }
+    
+    public static func setEmail(email: String){
+        UserDefaults.standard.set(email, forKey: emailKey)
+    }
+    
+    public static func getEmail() -> String? {
+        return UserDefaults.standard.string(forKey: emailKey)
+    }
+    
+    public static func setPwd(pwd: String){
+        UserDefaults.standard.set(pwd, forKey: pwdKey)
+    }
+    
+    public static func getPwd() -> String? {
+        return UserDefaults.standard.string(forKey: pwdKey)
+    }
+    
 }
